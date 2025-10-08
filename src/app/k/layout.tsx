16bdb2e1f9065fbca30f2aec26e5ee9fcdc3af2a
devhatebug <togiap.dev@gmail.com>
2025-10-08 19:18:45 +0700

    feat: add favicon and update layout for Examio platform
    - Added favicon.svg for the application.
    - Created a new layout component for the 'k' section with a sidebar.
    - Implemented a new page component for the 'k' section.
    - Updated the main layout metadata to reflect the Examio branding.
    - Modified the Logo component to use the new favicon and added animation.
    - Developed a SidebarKit component with navigation links for various tools.
    - Introduced a custom SeparatorPro component for better UI separation.

---
+++ src/app/k/layout.tsx
@@ -0,0 +1,25 @@
+import { SidebarKit } from "@/components/organisms/k/SideBar";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Examio - Quản lý",
+  description: "Nền tảng ôn tập và thi trực tuyến hỗ trợ bởi AI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div
+      className={cn(
+        "mx-auto flex w-full min-h-screen flex-1 overflow-hidden rounded-md flex-row ",
+        "h-[60vh] px-1 py-2"
+      )}>
+      <SidebarKit />
+      {children}
+    </div>
+  );
+}
---
+++ src/components/organisms/k/SideBar.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { LogoOnly } from "@/components/atoms/Logo";
+import { Bot, BookOpenCheck, SquareSplitVertical, Gem } from "lucide-react";
+import Link from "next/link";
+import { SeparatorPro } from "@/components/ui/seperatorpro";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export function SidebarKit() {
+  const itemSiderbar = [
+    {
+      name: "Ai Tool",
+      href: "/k/ai-tool",
+      icon: <Bot size={20} />,
+      label: "AI",
+    },
+    {
+      name: "Flash Card",
+      href: "/k/flash-card",
+      icon: <SquareSplitVertical size={20} />,
+      label: "Thẻ nhớ",
+    },
+    {
+      name: "Manage Exam",
+      href: "/k/manage-exam",
+      icon: <BookOpenCheck size={20} />,
+      label: "Đề thi",
+    },
+    {
+      name: "Manage Exam Room",
+      href: "/k/manage-exam-room",
+      icon: <BookOpenCheck size={20} />,
+      label: "QL thi",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center justify-between gap-y-2 border border-1/2 rounded-lg h-full p-2">
+      <div className="flex flex-col items-center ">
+        <LogoOnly sizeIcon={30} />
+        <SeparatorPro variant="dots" className="w-8" />
+        <div className="flex flex-col gap-y-3 h-fit">
+          {itemSiderbar.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="flex flex-col items-center transition-colors p-1 rounded-md group max-w-14">
+              <span className="p-1 rounded-md transition-colors group-hover:bg-accent group-hover:text-accent-foreground">
+                {item.icon}
+              </span>
+              <span className="text-[13px] font-medium text-center text-nowrap">
+                {item.label}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center ">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="secondary"
+              size="icon"
+              className="p-1 rounded-md cursor-pointer">
+              <span className="p-1 rounded-md transition-colors group-hover:bg-accent group-hover:text-accent-foreground">
+                <Gem size={20} className="text-blue-500" />
+              </span>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Nâng cấp tài khoản</TooltipContent>
+        </Tooltip>
+      </div>
+    </div>
+  );
+}
