013cc8b18655ab8bee7c6e1aad2d28832a965ae5
cychipo <togiap.dev@gmail.com>
2025-12-29 10:55:57 +0700

    feat: increase dialog content width in ImageUrlDialog component

--- src/components/organisms/ImageUrlDialog.tsx
+++ src/components/organisms/ImageUrlDialog.tsx
@@ -125,7 +125,7 @@ export function ImageUrlDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="sm:max-w-xl">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             <ImageIcon className="h-5 w-5" />
